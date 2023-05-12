c26b21cb80a78ef02c51e41305d4d5bdd1c01a8b
Duong Tuan Hung <duongtuanhung@Duongs-MacBook-Pro.local>
2023-05-12 10:02:31 +0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { Table } from 'react-bootstrap';
+import parse from 'html-react-parser';
+
+const socket = io('http://localhost:8001');
 
 function App() {
+  const [dataTranding, setData] = useState([]);
+  const [oldData, setOldData] = useState([]);
+  useEffect(() => {
+    // Socket.io events
+    socket.on('connect', () => {
+      console.log('Connected to server!');
+    });
+
+    socket.on('detect-profit', (data) => {
+      if (data.length === 0) {
+        setData(oldData);
+      } else {
+        setData(data);
+        setOldData(data);
+      }
+      console.log('Received data:', data);
+    });
+
+    // Clean up
+    return () => {
+      socket.off('connect');
+      socket.off('message');
+    };
+  }, [oldData]);
+
+  // function handleClick() {
+  //   socket.emit('message', 'Hello, server!');
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Binance Triangular Arbitrage Scanner</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th># No</th>
+            <th>Opportunity pairs</th>
+            <th>Profit(%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataTranding.map((item, index) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{parse(item.tpath)}</td>
+              <td>{item.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
